7659ae168e94044173e6b61f6a03559aea88a147
mind0n <markyuanmj@126.com>
2020-09-26 22:43:15 +0800

    reset

---
+++ dist/deep/deeper/bar.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Bar = /** @class */ (function () {
+    function Bar() {
+    }
+    return Bar;
+}());
+exports.Bar = Bar;
+//# sourceMappingURL=bar.js.map
---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+// Sample exports from other modules
+__export(require("./deep/foo"));
+__export(require("./deep/deeper/bar"));
+// Sample export of our own
+exports.bas = "123";
+//# sourceMappingURL=index.js.map
